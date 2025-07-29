9134bd5916a1be15587aafd59dfb905381bdccdb
yuanzhen <yuanzhenwork@qq.com>
2025-07-29 16:47:21 +0800

    refactor(captcha_image): 优化验证码生成逻辑，移除冗余代码

--- app/utils/captcha_image.py
+++ app/utils/captcha_image.py
@@ -1,35 +1,26 @@
 import base64
 import random
 import string
-import time
 from io import BytesIO
 
 from captcha.image import ImageCaptcha
 
 
 # 生成图片验证码
-def generate_captcha_image(uuid):
+def generate_captcha_image(img_format="PNG"):
     """
     生成图片验证码
-    :param uuid: 用户id
     :return: 验证码base64格式图片
     """
-    chr_all = string.ascii_letters + string.digits  # 所有字符，包括大小写和数字，用于生成验证码。
-    chr_4 = ''.join(random.sample(chr_all, 4))
-    image = ImageCaptcha().generate_image(chr_4)
-    save_captcha(uuid, chr_4, str(round(time.time() * 1000)))
-    return pil_base64(image)
+    chars = ''.join(random.sample(string.ascii_letters + string.digits, 4))
+    img = ImageCaptcha(width=120, height=40).generate_image(chars)
+    buf = BytesIO()
+    img.save(buf, format=img_format)
+    b64 = base64.b64encode(buf.getvalue()).decode()
+    return f'data:image/{img_format.lower()};base64,{b64}'
 
 
-# PIL转base64
-def pil_base64(image):
-    img_buffer = BytesIO()
-    image.save(img_buffer, format='JPEG')
-    byte_data = img_buffer.getvalue()
-    base64_str = base64.b64encode(byte_data)
-    return base64_str
 
-
-# 将验证码插入数据表
-def save_captcha(uuid, code, expire_time):
-    return None
+# # 将验证码插入数据表
+# def save_captcha(uuid, code, expire_time):
+#     return None
